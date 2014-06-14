821dc843c2884b8c0c89df36090710a7e0d34604
Daniel Beasley <danbzly@gmail.com>
2014-06-14 17:21:08 -0400

    created blog scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614164703) do
+ActiveRecord::Schema.define(version: 20140614204150) do
+
+  create_table "blogs", force: true do |t|
+    t.string   "title"
+    t.string   "category"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
