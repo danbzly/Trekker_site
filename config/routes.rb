c04a0c3976ca3421bf865f3bd70499183cc268b1
Daniel Beasley <danbzly@gmail.com>
2014-07-05 12:17:48 -0400

    home page changes and added insagram page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "blogs#index"
   get "about" => "pages#about"
+  get "instagram" => "pages#instagram"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
