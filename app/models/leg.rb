bafb2b4892150cd448d633381bc7d141114f974f
Daniel Beasley <danbzly@gmail.com>
2014-08-13 01:13:21 -0400

    changed out photo

--- app/models/leg.rb
+++ app/models/leg.rb
@@ -4,5 +4,8 @@ belongs_to :user
   has_attached_file :map_img, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :map_img, :content_type => /\Aimage\/.*\Z/
   
- 
+ def to_param
+    "#{id}-#{leg_header}"
+  end
+  
 end
